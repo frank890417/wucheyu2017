ca344b68dc729d3d9e5451d6f8ce78674eed9e0e
frank890417 <frank890417@gmail.com>
2019-01-23 00:27:55 +0800

    update navbar hide index

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ export default new Router({
     {
       path: '/',
       name: 'index',
-      component: pageIndex
+      component: pageWorks
     }, {
       path: '/about',
       name: 'about',
