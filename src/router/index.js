c59478392335fa16a1bf3c3c9788c128c6e7382e
frank890417 <frank890417@gmail.com>
2019-05-03 19:42:44 -0400

    update website to history mode

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ import pageProjectEdit from '@/components/pages/pageProjectEdit'
 Vue.use(Router)
 
 export default new Router({
-  // mode: 'history',
+  mode: 'history',
   routes: [
     {
       path: '/',
