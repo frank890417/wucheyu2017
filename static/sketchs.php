72fdf4faf236fb63a8954d5b640c148cc2606de6
frank890417 <frank890417@gmail.com>
2020-05-11 12:15:02 -0400

    remove art php json cache

--- static/sketchs.php
+++ static/sketchs.php
@@ -1,4 +1,8 @@
 <?php
+
+  header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+  header("Cache-Control: post-check=0, pre-check=0", false);
+  header("Pragma: no-cache");
   header('Content-type: application/json');
   $data = file_get_contents('https://www.openprocessing.org/api/user/139364/sketchs.json');
   echo $data;
